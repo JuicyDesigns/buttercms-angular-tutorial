853d6513476f33d263f4134115ec83cc694bfd19
JuicyDesigns <info@juicydesigns.co.za>
2022-03-08 22:17:05 +0200

    Update butterCMS.service.ts

--- src/app/services/butterCMS.service.ts
+++ src/app/services/butterCMS.service.ts
@@ -1,4 +1,4 @@
 import Butter from 'buttercms';
 
-export const butterService = Butter('your_api_token');
+export const butterService = Butter('09a6e5cf5ae5d77da64178b9dee1cb3aa064d24d');
 // replace `your_api_token` with the token provided to you by ButterCMS
